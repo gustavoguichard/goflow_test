21e715e2ab08f8e77aab56048e0bf463b64e6fa6
Gustavo Guichard <gustavoguichard@gmail.com>
2013-07-15 20:43:55 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoflowTest::Application.config.secret_token = '0db086960736bcb4dad58b07448c00e590c7d0830c9c654a8a13304953daaf3930dad487889b619dbf883057de78f9c4da6aa1968948a8f26c91e3cb4b37b4bb'
