7b74213b0044c358cdad3a40db10f37ecdc757c3
Gustavo Guichard <gustavoguichard@gmail.com>
2013-07-19 02:00:01 -0300

    Depending on the environment now

--- config/initializers/01-load_config.rb
+++ config/initializers/01-load_config.rb
@@ -1,6 +1,5 @@
-CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
-
 unless Rails.env.production?
+  CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
   ENV["DATABASE_URL"]        = CONFIG["DATABASE_URL"]
   ENV["DATABASE_NAME"]    = CONFIG["DATABASE_NAME"]
 end
